46de2244e4f7e02342dc233e295b9795bf3efc88
“[firstname <[valid-email]>
2022-05-08 19:44:16 +0300

    first commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.helloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnBmi: Button
+    lateinit var btnSendMoney:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnBmi = findViewById(R.id.btnBmi)
+        btnBmi.setOnClickListener {
+         val intent = Intent(this,BMIcalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSendMoney = findViewById(R.id.btnSendMoney)
+        btnSendMoney.setOnClickListener{
+         val intent = Intent(this,sendmoney::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
